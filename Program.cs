d871429b0bcb6f46cabea8cbf0d413dabf0aada8
NievaS24 <santinieva24@gmail.com>
2025-05-22 21:10:31 -0300

    Ejercicio 1 terminado

--- Program.cs
+++ Program.cs
@@ -7,3 +7,21 @@ a = 10;
 b = a;
 Console.WriteLine("valor de a:" + a);
 Console.WriteLine("valor de b:" + b);
+
+//Ejercicio 1
+int num, aux = 0;
+string input;
+do
+{
+    Console.WriteLine("Ingrese un numero");
+    input = Console.ReadLine();
+} while (!int.TryParse(input, out num));
+if (num > 0)
+{
+    while (num > 0)
+    {
+        aux = aux * 10 + num % 10;
+        num /= 10;
+    }
+}
+Console.WriteLine(aux);
